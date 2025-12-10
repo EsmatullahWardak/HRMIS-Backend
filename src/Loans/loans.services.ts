cb9711d55058d0d2ae3e9962c678576a20938ef0
Esmatullah Elham <115543724+EsmatullahWardak@users.noreply.github.com>
2025-12-10 19:09:59 +0430

    new changes applied

---
+++ src/Loans/loans.services.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateLoanDto } from './dto/create-loan.dto';
+
+@Injectable()
+export class LoansService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createLoan(createLoanDto: CreateLoanDto) {
+    return this.prisma.loan.create({
+      data: {
+        type: createLoanDto.type,
+        amount: createLoanDto.amount,
+        remaining: createLoanDto.remaining,
+        monthlyDeduction: createLoanDto.monthlyDeduction,
+        status: createLoanDto.status || 'Pending',
+        guarantor: createLoanDto.guarantor || null,
+        notes: createLoanDto.notes || null,
+        progress: createLoanDto.progress || 0,
+        userId: createLoanDto.userId || null,
+      },
+    });
+  }
+
+  async getAllLoans() {
+    return this.prisma.loan.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+}
