3d16341067132ec11c611a5a7b96c4950ac4cbef
Esmatullah Elham <115543724+EsmatullahWardak@users.noreply.github.com>
2025-12-15 18:07:59 +0430

    module created for leave section in database

---
+++ prisma/migrations/20251215133704_add_leave_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Leave" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "reason" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'Pending',
+    "userId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Leave_pkey" PRIMARY KEY ("id")
+);
