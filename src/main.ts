927d075e6187eac26c8a2e83651b21b5ca649f24
Esmatullah Elham <115543724+EsmatullahWardak@users.noreply.github.com>
2025-12-15 18:48:05 +0430

    in main.js file chagnes are applied

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:3002',
-      'http://192.168.6.202:3000',
-    ],
+    origin: ['http://localhost:3000', 'http://192.168.6.202:3000'],
     credentials: true,
   });
 
